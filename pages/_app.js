9771f0e3ff257969aae8470e8de207d38c20f278
vitormiura <vitormiura3@gmail.com>
2022-05-11 21:34:09 -0300

    first page!

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/globals.css';
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"></link>
+
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Home(){
+    return( 
+        <html>
+            <head>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"></link>
+            </head>
+            <div>
+                <h1>Goals List:</h1>
+                <ul>
+                    <li>Use frameworks as water.css or tailwind for styling</li>
+                    <li>About-me, Feed and Contact Pages</li>
+                    <li>Integrate with Django in the future</li>
+                    <li>Buy my own domain</li>
+                    <li>Use a github repo as a post method (maybe)</li>
+                    <li>Long life to all monkeys in the whole world! 🐒</li>
+                </ul>
+                <img src="https://c.tenor.com/s6YMqOTrKiwAAAAS/beijo-macaco.gif" width="500" height="600"></img>
+            </div>
+        </html>
+    )
+}
+
+
+export default Home
