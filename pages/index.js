4760f7833c5f82ec80eea1c4e29a42918091f532
vitormiura <vitormiura3@gmail.com>
2022-05-11 21:46:53 -0300

    fixing some dumb grammar errors

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ function Home(){
             <div>
                 <h1>Goals List:</h1>
                 <ul>
-                    <li>Use frameworks as water.css or tailwind for styling</li>
+                    <li>Use frameworks such as water.css or tailwind for styling</li>
                     <li>About-me, Feed and Contact Pages</li>
                     <li>Integrate with Django in the future</li>
                     <li>Buy my own domain</li>
                     <li>Use a github repo as a post method (maybe)</li>
-                    <li>Long life to all monkeys in the whole world! 🐒</li>
+                    <li>Long-life to all monkeys in the whole world! 🐒</li>
                 </ul>
                 <img src="https://c.tenor.com/s6YMqOTrKiwAAAAS/beijo-macaco.gif" width="500" height="600"></img>
             </div>
