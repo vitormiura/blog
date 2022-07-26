2023b6224064686bb10994772a47b05579778a29
vitormiura <vitormiura3@gmail.com>
2022-07-25 22:25:27 -0300

    change project structure to vite vanilla

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/globals.css';
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"></link>
-
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
